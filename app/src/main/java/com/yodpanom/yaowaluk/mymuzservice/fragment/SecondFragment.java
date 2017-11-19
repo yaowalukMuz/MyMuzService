916ba469d2648cd2fc9900753b163bcc3b413479
yaowalukMuz <yaowalukMuz.c@gmail.com>
2017-11-19 14:01:47 +0700

    show date success

--- app/src/main/java/com/yodpanom/yaowaluk/mymuzservice/fragment/SecondFragment.java
+++ app/src/main/java/com/yodpanom/yaowaluk/mymuzservice/fragment/SecondFragment.java
@@ -3,17 +3,88 @@ package com.yodpanom.yaowaluk.mymuzservice.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.yodpanom.yaowaluk.mymuzservice.R;
+import com.yodpanom.yaowaluk.mymuzservice.utility.GetJSON;
+import com.yodpanom.yaowaluk.mymuzservice.utility.MyConstant;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Created by Yaowaluk on 18/11/2560.
  */
 
 public class SecondFragment extends Fragment{
+
+    //Explicit
+    private String jsonString,rateString,dateString,answerString, usdString;
+    private String jsonRateString;
+    private Double rateADouble;
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //Read JSON
+        readJSON();
+
+        //show date from json data
+        showDate();
+
+
+
+    }// Main Method
+
+    private void showDate() {
+        TextView textView = getView().findViewById(R.id.txtShowDate);
+
+        try {
+            JSONArray jsonArray = new JSONArray("[" + jsonString + "]");
+            JSONObject jsonObject = jsonArray.getJSONObject(0);
+            dateString = jsonObject.getString("date");
+            Log.d("19NovV1", "Date____>" + " " + dateString);
+
+            textView.setText(getText(R.string.date) + dateString);
+
+            jsonRateString = "[" + jsonObject.getString("rates") + "]";
+
+            Log.d("19NovV1", "jsonRateString____>" + " " + jsonRateString);
+
+            JSONArray jsonArray1 = new JSONArray(jsonRateString);
+            JSONObject jsonObject1 = jsonArray1.getJSONObject(0);
+            rateString = jsonObject1.getString("THB");
+            Log.d("19NovV1", "RateString____>" + " " + rateString);
+
+            textV
+
+
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+    }
+
+    private void readJSON() {
+        String tag="19NovV1";
+        MyConstant myConstant = new MyConstant();
+        try {
+            GetJSON getJSON = new GetJSON(getActivity());
+            getJSON.execute(myConstant.getUrlJSON());
+            jsonString = getJSON.get();
+            Log.d(tag, "JSON==>" + jsonString);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d(tag, "e ==>" + e.toString());
+        }
+
+    }
+
+    // Create view (Make Mask)
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
--- app/src/main/java/com/yodpanom/yaowaluk/mymuzservice/utility/GetJSON.java
+++ app/src/main/java/com/yodpanom/yaowaluk/mymuzservice/utility/GetJSON.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 /**
  * Created by Yaowaluk on 19/11/2560.
@@ -26,13 +27,23 @@ public class GetJSON extends AsyncTask<String,Void,String> {
             OkHttpClient okHttpClient = new OkHttpClient();
             Request.Builder builder = new Request.Builder();
             Request request = builder.url(strings[0]).build();
+            Response response = okHttpClient.newCall(request).execute();
+
+            return response.body().string();
 
         } catch (Exception e) {
-        e.printStackTrace();
-
+            e.printStackTrace();
+            return null;
         }
 
+    }
 
-        return null;
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
+
+
     }
 }// main class
