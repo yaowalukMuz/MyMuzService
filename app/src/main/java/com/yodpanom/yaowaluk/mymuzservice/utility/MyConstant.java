e3dc292a4ac15035b01a4063f07e532e15b67656
yaowalukMuz <yaowalukMuz.c@gmail.com>
2017-11-19 11:09:10 +0700

    call okhttp.getJSON

---
+++ app/src/main/java/com/yodpanom/yaowaluk/mymuzservice/utility/MyConstant.java
@@ -0,0 +1,17 @@
+package com.yodpanom.yaowaluk.mymuzservice.utility;
+
+/**
+ * Created by Yaowaluk on 19/11/2560.
+ */
+
+public class MyConstant {
+
+    //    Explicit
+    private String urlJSON = "https://api.fixer.io/latest?symbols=THB&base=USD";
+
+
+    public String getUrlJSON() {
+        return urlJSON;
+    }
+
+}//MyConstant Class
